1d6454b69900fd8d4de2adc6993b47ac7f59bb9e
Simon Sellar <simon.sellar@ersa.edu.au>
2014-07-09 17:07:11 +0930

    Worked on git functions.

--- rebase_repo.py
+++ rebase_repo.py
@@ -26,7 +26,10 @@ class App:
         self.startingbranch = ''
         self.root = Tk()
         self.remotenames = []
+        self.currentstatus = StringVar()
         self.rebaseremote = StringVar()
+        self.rebase_button = ''
+        self.quit_button = ''
 
         self.rebasecurrentbranch = IntVar()
         self.rebasecurrentbranch.set(0)
@@ -37,12 +40,14 @@ class App:
         # Gather required information
         self.getcurrentbranchname()
         self.getremotenames()
+        self.getlocalbranches()
 
     def getcurrentbranchname(self):
         """
         Get the current branch name
         """
 
+        self.update_status('Getting current branch name')
         branchname = re.compile(r"^On branch (?P<branchname>.*?)\n")
         try:
             # Run the git status command
@@ -50,6 +55,22 @@ class App:
             output = p.communicate()[0]
             self.startingbranch = branchname\
                 .search(output.decode('utf-8')).group('branchname')
+        except CalledProcessError:
+            sys.stderr.write("Unable to get current branch name\n")
+            sys.exit(1)
+
+    def getlocalbranches(self):
+        """
+        Get a list of current branches.
+        """
+
+        self.update_status('Getting list of local branches')
+        branchname = re.compile(r"(?P<branchname>.*?)\n")
+        try:
+            # Run the git status command
+            p = Popen(['git', 'branch'], stdout=PIPE)
+            output = p.communicate()[0]
+            self.localbranches = output.decode('utf-8').replace('*', '').split()
         except CalledProcessError:
             sys.stderr.write("Unable to get current branch name\n")
             sys.exit(1)
@@ -65,16 +86,96 @@ class App:
         Rebases the using the given options
         """
 
+        # Disable buttons
+        self.quit_button['state'] = 'disabled'
+        self.rebase_button['state'] = 'disabled'
+
         # Debugging - Print out the known variables
         print('Starting Branch', self.startingbranch)
         print('Known remotes: ', ", ".join(self.remotenames))
-        print('Rebase to the following remote: ', self.rebaseremote.get())
-        print('Rebase my current branch as well?: ',
-              self.rebasecurrentbranch.get())
+        print(
+            'Rebase to the following remote: ',
+            self.rebaseremote.get()
+        )
+        print(
+            'Rebase my current branch as well?: ',
+            self.rebasecurrentbranch.get()
+        )
         print('Push to my origin repo?: ', self.pushtoorigin.get())
 
-        self.quit()
+        self.update_status('Checking out master branch')
 
+        # Go to our master branch if we are not already there
+        if self.startingbranch != 'master':
+            self.checkout('master')
+
+        # Fetch the other remote repositories
+        self.fetchremote('upstream')
+        self.fetchremote('origin')
+
+        try:
+            # Run the master rebase
+            p = Popen(
+                [
+                    'git',
+                    'rebase',
+                    self.rebaseremote.get() + '/master'
+                ],
+                stdout=PIPE
+            )
+            output = p.communicate()[0]
+
+        except CalledProcessError:
+            sys.stderr.write(
+                "Unable to rebase to "\
+                + self.rebaseremote.get()\
+                + "/master\n")
+            sys.exit(1)
+
+        # Go back to our initial branch
+        if self.startingbranch != 'master':
+            self.checkout(self.startingbranch)
+
+            if self.rebasecurrentbranch.get() == 1:
+                # Rebase our old branch too
+                print('Rebasing the current branch too')
+                try:
+                    # Run the master rebase
+                    p = Popen(
+                        [
+                            'git',
+                            'rebase',
+                            self.rebaseremote.get() + '/master'
+                        ],
+                        stdout=PIPE
+                    )
+                    output = p.communicate()[0]
+                    self.remotenames = output.decode('utf-8').split()
+                except CalledProcessError:
+                    sys.stderr.write(
+                        "Unable to rebase to "\
+                        + self.rebaseremote.get()\
+                        + "/master\n"
+                    )
+                    sys.exit(1)
+
+
+        self.update_status('Complete')
+
+        # Enable buttons
+        self.quit_button['state'] = 'enabled'
+        self.rebase_button['state'] = 'enabled'
+
+        # self.quit()
+
+    def update_status(self, statusmessage):
+        """
+        Forces an update of the UI status bar
+        """
+
+        self.currentstatus.set(statusmessage)
+        self.root.update()
+        
     def getremotenames(self):
         """
         Glean the git remote repos from the system
@@ -89,24 +190,70 @@ class App:
             sys.stderr.write("Unable to get remote repo names\n")
             sys.exit(1)
 
-    def creategui(self):
+    def fetchremote(self, remotename):
         """
-        Run the GUI
+        Fetch the latest DB from the given remote
+        """
+
+        self.update_status('Updating database from ' + remotename)
+        if remotename in self.remotenames:
+            try:
+                # Run the git status command
+                p = Popen(['git', 'fetch', '--prune', remotename])
+                output = p.communicate()[0]
+                if p.returncode != 0:
+                    sys.stderr.write("Unable to update " + remotename + "\n")
+                    self.quit(1)
+            except CalledProcessError:
+                sys.stderr.write("Unable to update " + remotename + "\n")
+                self.quit(1)
+
+    def checkout(self, branchname):
+       """
+       Changes the current repo into the nominated branch
+       """
+
+       # First check that the branch exists
+       if branchname in self.localbranches:
+           self.update_status('Checking out ' + branchname)
+           try:
+               # Run the git status command
+               p = Popen(['git', 'checkout', branchname])
+               output = p.communicate()[0]
+               if p.returncode != 0:
+                   sys.stderr.write("Unable to checkout " + branchname + "\n")
+                   self.quit(1)
+           except CalledProcessError:
+               sys.stderr.write("Unable to checkout " + branchname + "\n")
+               self.quit(1)
+
+    def run(self):
+        """
+        Run the program
         """
 
         # Set up the root window
         self.root.title(self.startingbranch)
+        self.root.rowconfigure(0, weight=1)
+        self.root.columnconfigure(0, weight=1)
 
-        mainframe = ttk.Frame(self.root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        mainframe.columnconfigure(0, weight=1)
+        mainframe = ttk.Frame(
+            self.root,
+            padding="3 3 3 3"
+        )
         mainframe.rowconfigure(0, weight=1)
+        mainframe.columnconfigure(0, weight=1)
+        mainframe.grid(
+            column=0,
+            row=0,
+            sticky=(N, W, E, S)
+        )
 
         # Notify of the current branch
         ttk.Label(mainframe, text='Current Branch:').grid(
             column=1,
             row=1,
-            sticky=(W, E)
+            sticky=W
         )
         ttk.Label(
             mainframe,
@@ -114,12 +261,12 @@ class App:
             foreground='red').grid(
                 column=2,
                 row=1,
-                sticky=(W, E))
+                sticky=W)
 
         # Remote to rebase against
         ttk.Label(mainframe,
                   text='Rebase Against:')\
-            .grid(column=1, row=2, sticky=(W, E))
+            .grid(column=1, row=2, sticky=W)
 
         cb1 = ttk.Combobox(mainframe,
                            textvariable=self.rebaseremote,
@@ -127,7 +274,7 @@ class App:
                            state='readonly')
         cb1.grid(column=2,
                  row=2,
-                 sticky=(W, E))
+                 sticky=W)
 
         # If upstream exists, set it to the default
         if 'upstream' in self.remotenames:
@@ -162,16 +309,51 @@ class App:
                  row=4,
                  sticky=E)
 
-        # Add buttons at the bottom
-        ttk.Button(mainframe,
-                   text='Quit',
-                   command=self.quit)\
-            .grid(column=1, row=5, sticky=W)
+        # Separator
+        ttk.Separator(mainframe).grid(
+            column=1,
+            row=5,
+            sticky=(W, E),
+            columnspan=2
+        )
 
-        ttk.Button(mainframe,
-                   text='Rebase',
-                   command=self.rebase)\
-            .grid(column=2, row=5, sticky=E)
+        # Status bar
+        ttk.Label(mainframe,
+                  textvariable=self.currentstatus)\
+                  .grid(
+                      column=1,
+                      row=6,
+                      sticky=W,
+                      columnspan=2
+                  )
+
+        # Separator
+        ttk.Separator(mainframe).grid(
+            column=1,
+            row=7,
+            sticky=(W, E),
+            columnspan=2
+        )
+
+        # Add buttons at the bottom
+        self.quit_button = ttk.Button(
+            mainframe,
+            text='Quit',
+            command=self.quit
+        )
+        self.quit_button.grid(column=1, row=8, sticky=W)
+
+        self.rebase_button = ttk.Button(
+            mainframe,
+            text='Rebase',
+            command=self.rebase
+        )
+        self.rebase_button.grid(column=2, row=8, sticky=E)
+
+        for x in range(2):
+            mainframe.columnconfigure(x, weight=1)
+        for y in range(8):
+            mainframe.rowconfigure(y, weight=1)
 
         # feet = StringVar()
         # meters = StringVar()
@@ -205,5 +387,5 @@ class App:
 if __name__ == '__main__':
 
     app = App()
-    app.creategui()
+    app.run()
 
